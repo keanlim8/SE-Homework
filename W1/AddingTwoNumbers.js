e76c6a78e4133eea1556891dcdddb9e1990f4e32
keanlim8 <keanlim58@gmail.com>
2020-12-19 19:15:53 -0500

    add week 1 files

---
+++ W1/AddingTwoNumbers.js
@@ -0,0 +1,18 @@
+// Week 1 Day 4 Morning Lab
+var num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 ;
+num1=3;
+num2=2;
+num3=9;
+num4=7;
+num5=22;
+num6=17;
+num7=15;
+num8=6;
+num9=1;
+num10=12;
+var sum1 = (num1 + num2 + num3 + num4 + num5) / 5;
+var sum2 = (num6 + num7 + num8 + num9 + num10) / 5;
+var sum = (sum1 + sum2) / 2;
+console.log(sum1);
+console.log(sum2);
+console.log(sum);
